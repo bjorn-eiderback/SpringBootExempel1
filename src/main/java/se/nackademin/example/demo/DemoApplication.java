bcb6fc0bfb682aad08ac82b3dfc8aed74b09363f
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2017-05-16 17:30:30 +0200

    Varför inte använda en lite snyggare loop...

--- src/main/java/se/nackademin/example/demo/DemoApplication.java
+++ src/main/java/se/nackademin/example/demo/DemoApplication.java
@@ -8,23 +8,22 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner demo(MinListaRepository repository) {
-		return (args) -> {
-			MinLista lista1 = new MinLista("Lista1");
-			Item item1 = new Item("Ett item");
-			lista1.getItems().add(item1);
-			repository.save(lista1);
+    @Bean
+    public CommandLineRunner demo(MinListaRepository repository) {
+        return (args) -> {
+            MinLista lista1 = new MinLista("Lista1");
+            Item item1 = new Item("Ett item");
+            lista1.getItems().add(item1);
+            repository.save(lista1);
 
-			for(MinLista lista : repository.findAll())
-			{
-				System.out.println("Listans namn: " + lista.getName());
-				lista.getItems().forEach(item -> System.out.println("Item: " + item.getName()));
-			}
-		};
-	}
+            repository.findAll().forEach(lista -> {
+                System.out.println("Listans namn: " + lista.getName());
+                lista.getItems().forEach(item -> System.out.println("Item: " + item.getName()));
+            });
+        };
+    }
 }
