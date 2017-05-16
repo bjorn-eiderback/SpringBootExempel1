89015381be823f3b31c2daca1081ca6454918ec7
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2017-05-16 14:32:23 +0200

    Ett test med en "query" + rensa innan varje test

--- src/test/java/se/nackademin/example/demo/DemoApplicationTests.java
+++ src/test/java/se/nackademin/example/demo/DemoApplicationTests.java
@@ -1,11 +1,13 @@
 package se.nackademin.example.demo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import javax.persistence.*;
 
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +21,11 @@ public class DemoApplicationTests {
 	@Autowired
 	private MinListaRepository repo;
 
+	@Before
+	public void init() {
+		clear();
+	}
+
 	@Test
 	public void testFindFirstName() {
 		MinLista lista1 = new MinLista("Lista1");
@@ -29,6 +36,32 @@ public class DemoApplicationTests {
 		List<MinLista> findByNameLista = repo.findByName((lista1.getName()));
 
 		assertThat(findByNameLista).extracting(MinLista::getName).containsOnly(lista1.getName());
+	}
+
+	@Test
+	public void testQuery() {
+		MinLista lista1 = new MinLista("Lista1");
+		Item item1 = new Item("Ett item");
+		lista1.getItems().add(item1);
+		entityManager.persist(lista1);
+		Query query = entityManager.getEntityManager().createQuery("SELECT item FROM Item item");
+
+		assertThat(query.getResultList().size()).isEqualTo(1);
+	}
+
+	private void clear() {
+		clearMinLista();
+	}
+
+	private void clearMinLista() {
+		List<MinLista> resultList = (List<MinLista>) repo.findAll();
+		try {
+			for (MinLista next : resultList) {
+				repo.delete(next);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
